49cbd755442b903e6f3c62f235f66d3249a26bd4
Drake Song <drakesong216@gmail.com>
2017-05-31 04:22:17 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,18 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-       redirect_to new_contact_path, notice: "Message sent."
+      redirect_to new_contact_path, notice: "Message sent."
     else
-       redirect_to new_contact_path, notice: "Error occured."
+      redirect_to new_contact_path, notice: "Error occured."
     end
   end
+  
   private
     def contact_params
-       params.require(:contact).permit(:name, :email, :comments)
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
